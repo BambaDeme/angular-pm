5c3d056e6d153b20dae50afcd2ed234290fd2a01
bambademe <cheikhbambademe@gmail.com>
2021-11-21 05:39:05 -0800

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 const appRoutes: Routes = [
-  {
-    path:'products',component:ProductListComponent,
-  },
-  {
-    path:'products/:id',canActivate:[ProductDetailGuard],component:ProductDetailComponent,
-  },
   {
     path:'welcome',component:WelcomeComponent
   },
@@ -31,16 +20,13 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+   
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
